a103683ec3eeb9fe01706ab0a735f4438930c94d
monberin <hnatenko@ucu.edu.ua>
2020-11-21 19:30:49 +0200

    arraylist + tests

--- src/main/java/ua/edu/ucu/collections/immutable/ImmutableArrayList.java
+++ src/main/java/ua/edu/ucu/collections/immutable/ImmutableArrayList.java
@@ -1,5 +1,101 @@
 package ua.edu.ucu.collections.immutable;
 
-public class ImmutableArrayList {
-    
+public class ImmutableArrayList implements ImmutableList {
+    private Object[] array = {};
+    private int size = 0;
+
+    public ImmutableArrayList(){
+
+    }
+
+    public ImmutableArrayList(Object[] list){
+        this.array = list;
+        this.size = list.length;
+    }
+
+    @Override
+    public ImmutableArrayList add(Object e) {
+        return addAll(size, new Object[]{e});
+    }
+
+    void CheckIndexException(int index) {
+        if (index < 0 || index > size) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+    }
+
+    @Override
+    public ImmutableArrayList add(int index, Object e) {
+        return addAll(index, new Object[]{e});
+    }
+
+    @Override
+    public ImmutableArrayList addAll(Object[] c) {
+        return addAll(size, c);
+    }
+
+    @Override
+    public ImmutableArrayList addAll(int index, Object[] c) {
+        CheckIndexException(index);
+        Object[] arr = new Object[c.length + size];
+        System.arraycopy(c, 0, arr, index, c.length);
+        System.arraycopy(this.array, index, arr, index+c.length, size - index);
+        System.arraycopy(this.array, 0, arr, 0, index);
+        return new ImmutableArrayList(arr);
+    }
+
+    @Override
+    public Object get(int index) {
+        CheckIndexException(index);
+        return this.array[index];
+    }
+
+    @Override
+    public ImmutableArrayList remove(int index) {
+        CheckIndexException(index);
+        Object[] arr = new Object[size-1];
+        System.arraycopy(this.array, index+1, arr, index, size - (index+1));
+        System.arraycopy(this.array, 0, arr, 0, index);
+        return new ImmutableArrayList(arr);
+    }
+
+    @Override
+    public ImmutableArrayList set(int index, Object e) {
+        CheckIndexException(index);
+        Object[] arr = new Object[size];
+        System.arraycopy(this.array, index+1, arr, index+1, size - index-1);
+        System.arraycopy(this.array, 0, arr, 0, index);
+        arr[index] = e;
+        return new ImmutableArrayList(arr);
+    }
+
+    @Override
+    public int indexOf(Object e) {
+        for (int i = 0; i < size; i++){
+            if (this.array[i] == e){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    @Override
+    public ImmutableArrayList clear() {
+        return new ImmutableArrayList();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return this.array;
+    }
 }
--- src/test/java/ua/edu/ucu/collections/immutable/ImmutableArrayListTest.java
+++ src/test/java/ua/edu/ucu/collections/immutable/ImmutableArrayListTest.java
@@ -6,7 +6,103 @@ import static org.junit.Assert.*;
 public class ImmutableArrayListTest {
     
     @Test
-    public void testSomeMethod() {
+    public void testArrayListFromList() {
+        Integer[] arr = {0, 10, -5, 2};
+        ImmutableArrayList immutableArrayList = new ImmutableArrayList(arr);
+        assertEquals(immutableArrayList.get(0),0);
+        assertEquals(immutableArrayList.get(1), 10);
+        assertEquals(immutableArrayList.get(2),-5);
+        assertEquals(immutableArrayList.get(3), 2);
+
     }
-    
+
+    @Test
+    public void testAdd() {
+        Integer[] array = {0, 10, -5, 2};
+        ImmutableArrayList arr = new ImmutableArrayList(array);
+        ImmutableArrayList newArr = arr.add(3);
+        assertEquals((int) newArr.get(4), 3);
+    }
+
+    @Test
+    public void testAddIndex() {
+        Integer[] array = {0, 10, -5, 2};
+        ImmutableArrayList arr = new ImmutableArrayList(array);
+        ImmutableArrayList newArr = arr.add(1,3);
+        assertEquals((int) newArr.get(1), 3);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testAddIndexException() {
+        Integer[] array = {0, 10, -5, 2};
+        ImmutableArrayList arr = new ImmutableArrayList(array);
+        ImmutableArrayList newArr = arr.add(10,3);
+    }
+
+    @Test
+    public void testAddAll() {
+        Integer[] array = {0, 10, -5, 2};
+        Integer[] c = {4,0,2,7};
+        ImmutableArrayList arr = new ImmutableArrayList(array);
+        ImmutableArrayList newArr = arr.addAll(c);
+        assertEquals((int) newArr.get(6), 2);
+    }
+
+    @Test
+    public void testAddAllIndex() {
+        Integer[] array = {0, 10, -5, 2};
+        Integer[] c = {4,0,2,7};
+        ImmutableArrayList arr = new ImmutableArrayList(array);
+        ImmutableArrayList newArr = arr.addAll(1,c);
+        assertEquals((int) newArr.get(2), 0);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testAddAllIndexException() {
+        Integer[] array = {0, 10, -5, 2};
+        Integer[] c = {4,0,2,7};
+        ImmutableArrayList arr = new ImmutableArrayList(array);
+        ImmutableArrayList newArr = arr.add(10,3);
+    }
+
+    @Test
+    public void testRemove() {
+        Integer[] array = {0, 10, -5, 2};
+        ImmutableArrayList arr = new ImmutableArrayList(array);
+        ImmutableArrayList newArr = arr.remove(2);
+        assertEquals((int) newArr.get(2), 2);
+    }
+
+    @Test
+    public void testSet() {
+        Integer[] array = {0, 10, -5, 2};
+        ImmutableArrayList arr = new ImmutableArrayList(array);
+        ImmutableArrayList newArr = arr.set(3, 200);
+        assertEquals((int) newArr.get(3), 200);
+    }
+
+    @Test
+    public void testIndexOf() {
+        Integer[] array = {0, 10, -5, 2};
+        ImmutableArrayList arr = new ImmutableArrayList(array);
+        assertEquals(arr.indexOf(3), -1);
+        assertEquals(arr.indexOf(10), 1);
+    }
+
+    @Test
+    public void testIsEmpty() {
+        ImmutableArrayList list = new ImmutableArrayList();
+
+        boolean actualResult = list.isEmpty();
+        assertTrue(actualResult);
+    }
+
+    @Test
+    public void testToArray() {
+        Integer[] array = {0, 10, -5, 2};
+        ImmutableArrayList list = new ImmutableArrayList(array);
+        Object[] newArray = list.toArray();
+        assertArrayEquals(array, newArray);
+    }
+
 }
